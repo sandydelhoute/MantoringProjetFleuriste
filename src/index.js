9bb499b68fd06f32aea9fc01c6fc8b4230aaefe3
DoryanSens <doryan.sens@supinfo.com>
2023-08-24 19:16:32 +0200

    add router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import { createRoot } from 'react-dom/client';
-
-// Clear the existing HTML content
-document.body.innerHTML = '<div id="app"></div>';
+import React from "react"
+// import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { menus } from './constants/menu';
 
 // Render your React component instead
-const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world</h1>);
+const RoutesA = () => {
+    return <Routes>
+        {menus.map(
+            (menu, index) =>
+                <Route path={menu.path} exact={menu.exact} key={index}>
+                    <RoutesA.component a="toto" b="tata"/>
+                </Route>
+        )
+        }
+    </Routes>
+}
+
+const RouteRender = ()=><React.StrictMode>
+<Router >
+    <RoutesA />
+</Router>
+</React.StrictMode>
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(<RouteRender/>)
